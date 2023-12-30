f062a9459370ee7ab30ebb1019d0777bf441375b
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2023-12-30 16:13:51 -0600

    Justin Nguyen | HW2 Flask application

--- resources/cars/routes.py
+++ resources/cars/routes.py
@@ -4,29 +4,40 @@ from uuid import uuid4
 from app import app
 from db import cars, users
 
+@app.get('/cars')
+def carList():
+    return {'cars':cars}
+
+@app.get('/cars/<car_name>')
+def carSpec(car_name):
+    try:
+        return {f'{car_name}':cars[car_name]},200
+    except KeyError:
+        return {"message": "Invalid car"}, 400
+
 
 @app.post('/cars')
-def create_car():
-    post_data = request.get_json()   
-    user_id = post_data['user_id']
+def add_car():
+    car_data = request.get_json()   
+    user_id = car_data['user_id']
     if user_id in users:
-        cars[uuid4()]= post_data
-        return { 'message': "Post Created" }, 201
+        cars[uuid4()]= car_data
+        return { 'message': "Car Created" }, 201
     return { 'message': "Invalid User"}, 401
 
-@app.put('/post/<post_id>')
+@app.put('/car/<car_id>')
 def update_car(car_id):
     try:
         post = cars[car_id]
         post_data = request.get_json()
         if post_data['user_id']== post['user_id']:
             post['body'] = post_data['body']
-            return {'message': 'Post Updated'}, 202
+            return {'message': 'Car Updated'}, 202
         return {'message': "Unauthorized"}, 401
     except:
         return{'message': "Invalid Post"}, 400
     
-@app.delete('/post/<post_id>')
+@app.delete('/car/<car_id>')
 def delete_car(car_id):
     try:
         del cars[car_id]
