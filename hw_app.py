afe2d8dfa486325638a4d2b58383c25847b83a95
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2023-12-30 15:35:47 -0600

    Justin Nguyen | First Flask App

--- hw_app.py
+++ hw_app.py
@@ -42,60 +42,40 @@ cars = {
 }
 #  CRUD (Create(GET), Read(PUT), Update, Delete)
 
+# user routes
+
 @app.get('/user')
 def user():
-    return { 'users': list(users.values()) }, 200
+  return { 'users': list(users.values()) }, 200
 
 @app.route('/user', methods=["POST"])
 def create_user():
-    json_body = request.get_json()
-    users[uuid4()] = json_body
-    return { 'message' : f'{json_body["username"]} created'}, 201
+  json_body = request.get_json()
+  users[uuid4()] = json_body
+  return { 'message' : f'{json_body["username"]} created' }, 201
 
-@app.put('/user/<user_id>')
-def update_user(user_id):
-  try:
-    user = users[user_id]
-    user_data = request.get_json()
-    user |= user_data
-    return { 'message': f'{user["username"]} updated'}, 202
-  except KeyError:
-    return {'message': "Invalid User"}, 400
+@app.put('/user')
+def update_user():
+  return
 
-@app.delete('/user/<user_id>')
-def delete_user(user_id):
-  try:
-    del users[user_id]
-    return { 'message': f'User Deleted' }, 202
-  except:
-    return {'message': "Invalid username"}, 400
+@app.delete('/user')
+def delete_user():
+  pass
 
-@app.post('/cars')
+# post routes
+
+@app.get('/post')
+def get_posts():
+  return
+
+@app.post('/post')
 def create_post():
-    post_data = request.get_json()   
-    user_id = post_data['user_id']
-    if user_id in users:
-        cars[uuid4()]= post_data
-        return { 'message': "Post Created" }, 201
-    return { 'message': "Invalid User"}, 401
+  return
 
-@app.put('/post/<post_id>')
-def update_post(post_id):
-    try:
-        post = cars[post_id]
-        post_data = request.get_json()
-        if post_data['user_id']== post['user_id']:
-            post['body'] = post_data['body']
-            return {'message': 'Post Updated'}, 202
-        return {'message': "Unauthorized"}, 401
-    except:
-        return{'message': "Invalid Post"}, 400
-    
-@app.delete('/post/<post_id>')
-def delete_post(post_id):
-    try:
-        del cars[post_id]
-        return {"message": "Post Deleted"}, 202
-    except:
-        return {'message': "Invalid Post"}, 400
+@app.put('/post')
+def update_post():
+  return
 
+@app.delete('/post')
+def delete_post():
+  return
