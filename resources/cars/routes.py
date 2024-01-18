47fbbcedf82affe01dbd7f0d2473e074e1e1bfc7
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-17 19:00:50 -0600

    updated routes

--- resources/cars/routes.py
+++ resources/cars/routes.py
@@ -1,10 +1,11 @@
-    
 from flask import request
 from uuid import uuid4
 from flask.views import MethodView
+from flask_smorest import abort
 
+from models.PostModel import PostModel
 from schemas import PostSchema
-from db import cars, users
+from db import posts, users
 from . import bp
 # post routes
 
@@ -13,40 +14,40 @@ class Post(MethodView):
 
   @bp.response(200, PostSchema)
   def get(self, car_id):
-    try:
-      return cars[car_id]
-    except KeyError:
-      return {'message': "Invalid Post"}, 400
+    post = PostModel.query.get(car_id)
+    if post:
+      return post 
+    abort(400, message='Invalid Car')
 
   @bp.arguments(PostSchema)
   def put(self, car_data ,car_id):
-    try:
-      post = cars[car_id]
-      if car_data['user_id'] == cars['user_id']:
-        post['body'] = car_data['body']
-        return { 'message': 'Car Updated' }, 202
-      return {'message': "Unauthorized"}, 401
-    except:
-      return {'message': "Invalid Car Id"}, 400
+    post = PostModel.query.get(car_id)
+    if post:
+      post.body = car_data['body']
+      post.commit()
+    return {'message': "Invalid Car Id"}, 400
 
   def delete(self, car_id):
-    try:
-      del cars[car_id]
+    post = PostModel.query.get(car_id)
+    if post:
+      post.delete()
       return {"message": "Car Deleted"}, 202
-    except:
-      return {'message':"Invalid Car"}, 400
+    return {'message':"Invalid Car"}, 400
 
 @bp.route('/')
 class PostList(MethodView):
 
   @bp.response(200, PostSchema(many = True))
   def get(self):
-    return  list(cars.values())
+    return PostModel.query.all()
   
   @bp.arguments(PostSchema)
   def post(self, car_data):
-    user_id = car_data['user_id']
-    if user_id in users:
-      cars[uuid4()] = car_data
+    try:
+      post = PostModel()
+      post.user_id = car_data['user_id']
+      post.body = car_data['body']
+      post.commit()
       return { 'message': "Car Created" }, 201
-    return { 'message': "Invalid User"}, 401
+    except:
+      return { 'message': "Invalid User"}, 401
